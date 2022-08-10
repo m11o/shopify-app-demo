65768c95347ff2882777bbf77311333a8e47381a
m11o <m11o@hey.jp>
2022-08-10 21:56:00 +0900

    init commit

---
+++ web/app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  include ShopifyApp::EmbeddedApp
+  include ShopifyApp::RequireKnownShop
+  include ShopifyApp::ShopAccessScopesVerification
+
+  DEV_INDEX_PATH = Rails.root.join("frontend")
+  PROD_INDEX_PATH = Rails.public_path.join("dist")
+
+  def index
+    contents = File.read(File.join(Rails.env.production? ? PROD_INDEX_PATH : DEV_INDEX_PATH, "index.html"))
+
+    render(plain: contents, content_type: "text/html", layout: false)
+  end
+end
